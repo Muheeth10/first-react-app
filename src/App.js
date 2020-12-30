f65dd878acbe0b963da3b03af25b8f293e45757e
Muheeth10 <muheeth17@gmail.com>
2020-12-30 20:10:35 +0530

    dynamically updating the values

--- src/App.js
+++ src/App.js
@@ -1,42 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
 
-var mObj={
-  blogHeading:"Heading",
+var blogArr= [{
+  blogHeading:"Heading 1",
   para:'loro dolar espum loro dolar espum loro dolar espum loro dolar espum '
+},
+{
+  blogHeading:"Heading 2",
+  para:'body of the second card'
+},
+{
+  blogHeading:"Heading 3",
+  para:'body of the third card'
 }
+]
+
+const blogCard = blogArr.map((item, pos) => {
+  console.log(item)
+// using the pos for id is the bad practice use the objects or function for the id
+  return(
+    
+     <div className="BlogCard" key={pos}>
+       <h3>{item.blogHeading}</h3>
+       <p>{item.para}</p>
+     </div>
+
+  );
+})
 
 // style are two types inline and external. In inline objects are passed unlike the strings in the in javascript.
 // since style is an object it can be stored in the variable
 
-const boxstyle = {
-  maxWidth:'30%',
-  display:'inline-block',
-  margin:'16px',
-  padding:'16px',
-  backgroundColor:'lightblue',
-  borderRadius: '5px',
-  boxShadow:'0 2px 5px #ccc'
-}
+
 
 function App() {
  return(
    <div className="App">
-     <div style={boxstyle}>
-       <h3>{mObj.blogHeading}</h3>
-       <p>{mObj.para}</p>
-     </div>
-    
-     <div className="blogCard">
-       <h3>{mObj.blogHeading}</h3>
-       <p>{mObj.para}</p>
-     </div>
-    
-     <div className="blogCard">
-       <h3>{mObj.blogHeading}</h3>
-       <p>{mObj.para}</p>
-     </div>
-
+      {blogCard}
    </div>
  );
 }
